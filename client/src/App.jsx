d8d1a71e15d3693119f3473ef97a19a80199ec9b
parth5757 <psthakkar2@gmail.com>
2024-08-22 22:25:38 +0530

    frontend work completed & server also setuped

---
+++ client/src/App.jsx
@@ -0,0 +1,27 @@
+import { useState } from 'react';
+import './App.css';
+import {BrowserRouter , Routes, Route} from 'react-router-dom';
+import CreateUser from './CreateUser';
+import UpdateUser from './UpdateUser';
+import Users from './Users';
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <div>
+      <BrowserRouter>
+        <Routes>
+          <Route path='/' element={<Users />} ></Route>
+          <Route path='/create' element={<CreateUser/>} ></Route>
+          <Route path='/update' element={<UpdateUser />} ></Route>
+        </Routes>
+      </BrowserRouter>
+    </div>
+    // <>
+    // <p>parth</p>
+    // </>
+  )
+}
+
+export default App;
